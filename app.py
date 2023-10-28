fa20f4346e741790e15478938f0c2e6f3a29a059
Felipe Schossler <felipealvesschossler@gmail.com>
2023-10-28 11:13:14 -0300

    chore: comment better the app code for better understanding

--- app.py
+++ app.py
@@ -2,9 +2,12 @@ from flask import Flask, render_template
 
 app = Flask(__name__)
 
+# Add a /hello route
 @app.route('/hello')
 def hello():
+    # Return the index.html inside the templates folder, this is a pattern in Flask
     return render_template('index.html')
 
 if __name__ == '__main__':
+    # Start the app in the 8080 port
     app.run(port=8080)
